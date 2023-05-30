bd03d3141f275f37bc11816d4e2b172d49d22fa6
teste <88006072+izaiasbatis@users.noreply.github.com>
2023-05-30 20:34:55 -0300

    comite
    teste

---
+++ teste.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+
+cor1 = "#1e1f1e"
+
+janela = Tk()
+janela.title("Calculadora")
+janela.geometry("235x318")
+frame_janela = Frame(janela, width=250 , height=250, bg= cor1)
+frame_janela.grid(row=0, column=0)
+
+
+
+
+janela.mainloop()
